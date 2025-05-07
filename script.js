223a7d6efbf399cf0f5ea4b03669564e27090680
jordan <jsheeler90@gmail.com>
2025-05-06 21:40:05 -0400

    Created book display function, modal dialog
    Book display function loops through the myLibrary
    array and creates a div dom element with the text
    content being the bookinfo of the last book object,
    also added a remove button to the dom element. Currently
    the bookDisplay function is inside the addBookToLibrary
    function, I may change that later.
    Created a modal dialog with text inputs and a checkbox
    that calls the addBookToLibrary function with those values.

--- script.js
+++ script.js
@@ -10,27 +10,100 @@ this.author = author;
 this.read = read;
 
 this.bookInfo = function(){
-    return this.title + this.author + this.read;
+   return this.title + ", " + this.author + ", " + this.read;
 };
 };
 
 
 
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, read) {
 
-   
-    const usersNewBook = new Book('Jurassic Park, ', 'Michael Crichton, ', 'have read');
+let bookObject = new Book(title, author, read);
+bookObject.id = self.crypto.randomUUID();
+myLibrary.push(bookObject);
+
+
+
+
+function bookDisplay(){
     
+  for (i=0; i < myLibrary.length; i++){
+     const bookCard = document.createElement("div")
+     bookCard.setAttribute('data-book-id', bookObject.id);
+     const removeButton = document.createElement("button");
+     removeButton.textContent = "Remove";
+     
+     removeButton.addEventListener("click", function(){
+      bookCard.remove();
 
-    usersNewBook.id = self.crypto.randomUUID();
-
-    myLibrary.push(usersNewBook.bookInfo());
+      console.log(bookObject);
+      const index = myLibrary.indexOf(bookObject);
+      myLibrary.splice(index, 1);
     
-    console.log(usersNewBook);
+    });
+  
+     console.log(bookCard.dataset.bookId);
+  
+  if(myLibrary[i] === myLibrary[myLibrary.length -1]){
+   bookCard.textContent = myLibrary[myLibrary.length -1].bookInfo();
+   display.appendChild(bookCard);
+   bookCard.appendChild(removeButton);
+  }
+  }
+    console.log(myLibrary);
+    
+    
+  };
+bookDisplay();
 };
 
-addBookToLibrary();
 
-console.log(myLibrary);
+
+ 
+
+
+const display = document.querySelector("#book-display");
+
+
+const showButton = document.getElementById("show-dialog");
+const bookDialog = document.getElementById("book-dialog");
+const titleElement = bookDialog.querySelector("#title");
+const authorElement = bookDialog.querySelector("#author");
+const haveRead = bookDialog.querySelector("#have-read");
+const confirmBtn = bookDialog.querySelector("#confirm-btn");
+
+
+showButton.addEventListener("click", function(){
+bookDialog.showModal();
+});
+bookDialog.addEventListener("close", function(){
+  titleElement.value = "";
+  authorElement.value = "";
+});
+
+confirmBtn.addEventListener("click", function(event){
+  
+    if (haveRead.checked) {
+      readValue = "I've read this book!"
+    }
+    else{
+      readValue = "Haven't read it yet."
+    };
+
+if(titleElement.value === "" || authorElement.value === ""){
+  alert("please enter a book title and author.")
+  event.preventDefault();
+}
+else{
+    addBookToLibrary(titleElement.value, authorElement.value, readValue);
+    event.preventDefault();
+    bookDialog.close();
+    titleElement.value = "";
+    authorElement.value = "";}
+});
+
+
+
+
 
